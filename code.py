28b08aa2dad9fffb2ed2152e94d9d928addec743
Abdul-Hussein <136578029+Abdul-Hussein@users.noreply.github.com>
2023-09-19 10:56:32 +0000

    Tue, Sep 19, 2023, 10:56 AM +00:00

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
-list [0,1,2,3,0,4]
+list[0,2,3,6,7,0]
 for item in list:
-    if item==0:
-        list.remove=item
-        list.append=item
+    if item == 0:
+        list. remove(item)
+        list.append(item)
 print(list)
